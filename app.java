da920d8e37aaf445bfc2cd25b24e981d014fc601
Akanji3 <125752628+Akanji3@users.noreply.github.com>
2023-03-02 12:16:25 -0500

    Bug removed

--- app.java
+++ app.java
@@ -1,8 +1,9 @@
-we support 
-java applications
-.net application
-this is great
-developing java app
-.net applications
-notesjs applications
-python application
+we support; 
+java applications,
+.net application,
+this is great,
+developing java app,
+.net applications,
+notesjs applications,
+python application,
+bug is removed
